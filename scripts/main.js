2435ad55b4a9d4123998a9636fc3ab07888c5ae1
Gilbo11 <jackgj43@icloud.com>
2023-02-28 14:47:39 +0000

    more js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,19 @@
 
-document.getElementById("nope_btn").addEventListener("click", e=>{
-    document.getElementById("banner").style.display = "none";
+
+
+document.getElementById("wont_look").addEventListener("click", e=>{
+    document.getElementById("scrt_element").innerHTML = "You are probably not a Dragonborn";
+    document.getElementById("scrt_element").style.display = "block";
+    document.getElementById("game_1").style.display = "none";
 });
 
-document.getElementById("yes_btn").addEventListener("click", e=>{
-    document.getElementById("hey_msg").innerHTML = "Congratulations"
-    document.getElementById("banner_question").innerHTML = "Visit Skyrim now and we will set a dragon on you free of charge!"
-    document.getElementById("yes_btn").style.display = "none";
-    document.getElementById("nope_btn").innerHTML = "Yay!"
+document.getElementById("ignore").addEventListener("click", e=>{
+    document.getElementById("game_1").style.display = "none";
+    document.getElementById("game_2").style.display = "block";
 });
+
+document.getElementById("d_and_s").addEventListener("click", e=>{
+    document.getElementById("scrt_element").innerHTML = "You are probably not a Dragonborn";
+    document.getElementById("scrt_element").style.display = "block";
+    document.getElementById("game_2").style.display = "none";
+})
