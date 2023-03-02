38f20f3effac486d2d5a2d60f2a73c95ab3ee136
Jack Gilbert-Johns <5gilbj16@solent.ac.uk>
2023-03-02 12:42:41 +0000

    contact form

---
+++ scripts/contact_form.js
@@ -0,0 +1,9 @@
+document.getElementById("dragonborn").addEventListener("submit", e=>{
+    let nameinput = document.forms ["dragonborn"] ["txtname"].value;
+    
+        if (nameinput.trim() ==""){
+        alert("hello")
+        }
+
+        
+})
